2a44f998f4670fe38f92fb0c2dca62dde0b1b0d0
thatcherty <ryeking19@gmail.com>
2025-11-19 21:19:56 -0900

    restored mySQL app

---
+++ app/mySQL/SFILS/SFILS/Pages/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace SFILS.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SFILS.Pages.SFILS_Context _context;
+
+        public DetailsModel(SFILS.Pages.SFILS_Context context)
+        {
+            _context = context;
+        }
+
+        public Patron Patron { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patron = await _context.Patron.FirstOrDefaultAsync(m => m.Patron_Id == id);
+            if (patron == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Patron = patron;
+            }
+            return Page();
+        }
+    }
+}
